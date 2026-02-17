1070ebd17295b6acf956da0be29ed7067639d7e7
kofismartgh <90314355+kofismartgh@users.noreply.github.com>
2026-02-17 16:11:46 +0000

    Update import.tf

--- SG/import.tf
+++ SG/import.tf
@@ -1,3 +1,64 @@
 resource "aws_security_group" "my_imported_sg" {
-  # Leave this empty for now; we will fill it in after the import
+    description = "launch-wizard-8 created 2024-07-18T22:07:26.030Z"
+    egress      = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+    ]
+    ingress     = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 22
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 22
+        },
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 80
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 80
+        },
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = "redis"
+            from_port        = 6379
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 6380
+        },
+    ]
+    name        = "EC2-sg"
+    region      = "us-east-1"
+    tags        = {}
+    tags_all    = {}
+    vpc_id      = "vpc-07539f61"
 }
