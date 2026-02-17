2e9b4272fec5f39f9660cffb1f5cfe84f83aeb22
kofismartgh <90314355+kofismartgh@users.noreply.github.com>
2026-02-17 16:07:30 +0000

    Update import.tf

--- SG2/import.tf
+++ SG2/import.tf
@@ -20,7 +20,72 @@ provider "aws" {
 }
 
 
-import {
-  to = aws_security_group.cybersourceSG2
-  id = "sg-0f3bbadc059e1ebce"
-}
+#import {
+ # to = aws_security_group.cybersourceSG2
+ # id = "sg-0f3bbadc059e1ebce"
+#}
+
+resource "aws_security_group" "cybersourceSG2" {
+        description            = "launch-wizard-8 created 2024-07-18T22:07:26.030Z"
+        egress                 = [
+            {
+                cidr_blocks      = [
+                    "0.0.0.0/0",
+                ]
+                description      = ""
+                from_port        = 0
+                ipv6_cidr_blocks = []
+                prefix_list_ids  = []
+                protocol         = "-1"
+                security_groups  = []
+                self             = false
+                to_port          = 0
+            },
+        ]
+        ingress                = [
+            {
+                cidr_blocks      = [
+                    "0.0.0.0/0",
+                ]
+                description      = ""
+                from_port        = 22
+                ipv6_cidr_blocks = []
+                prefix_list_ids  = []
+                protocol         = "tcp"
+                security_groups  = []
+                self             = false
+                to_port          = 22
+            },
+            {
+                cidr_blocks      = [
+                    "0.0.0.0/0",
+                ]
+                description      = ""
+                from_port        = 80
+                ipv6_cidr_blocks = []
+                prefix_list_ids  = []
+                protocol         = "tcp"
+                security_groups  = []
+                self             = false
+                to_port          = 80
+            },
+            {
+                cidr_blocks      = [
+                    "0.0.0.0/0",
+                ]
+                description      = "redis"
+                from_port        = 6379
+                ipv6_cidr_blocks = []
+                prefix_list_ids  = []
+                protocol         = "tcp"
+                security_groups  = []
+                self             = false
+                to_port          = 6380
+            },
+        ]
+        name                   = "EC2-sg"
+        region                 = "us-east-1"
+      + revoke_rules_on_delete = false
+        tags                   = {}
+        vpc_id                 = "vpc-07539f61"
+    }
