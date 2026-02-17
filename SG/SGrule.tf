868173d7da60d4f5f5d340515f0706ca5aec1cd1
kofismartgh <90314355+kofismartgh@users.noreply.github.com>
2026-02-17 17:14:24 +0000

    Create SGrule.tf

---
+++ SG/SGrule.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group_rule" "allowto_access_cybersoucre_stage" {
+  type                     = "ingress"
+  from_port                = 9000
+  to_port                  = 9000
+  protocol                 = "tcp"
+  source_security_group_id = "sg-07d6266aada845680" #unifiedCheckoutApi 
+  description              = "From CheckoutWeb NewCheckoutApi NewCheckoutProxyApi SG to UnifiedCheckout"
+  security_group_id        = aws_security_group.cybersourceSG.id
+}
