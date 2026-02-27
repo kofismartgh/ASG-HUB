f555e45143625d5f0c4a195c87460bc00b71c7c5
kofismartgh <90314355+kofismartgh@users.noreply.github.com>
2026-02-27 13:08:39 +0000

    Update import.tf

--- SG/import.tf
+++ SG/import.tf
@@ -6,4 +6,12 @@ resource "aws_security_group" "my_imported_sg" {
     tags        = {}
     tags_all    = {}
     vpc_id      = "vpc-07539f61"
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    description      = "Allow all outgoing traffic"
+  }
 }
